683bfc02251788d150857259a5f0ec71460c6711
Jafet Suárez <jafet.suarezxbox@hotmail.com>
2020-12-15 10:44:39 -0600

    README pares y rendimiento

--- load/locust_files/locustfile.py
+++ load/locust_files/locustfile.py
@@ -4,14 +4,14 @@ from locust import HttpUser, between, task
 class WebsiteUser(HttpUser):
     wait_time = between(5, 15)
     
-    def on_start(self):
-        self.client.get("/marcas-y-productos/")
+    #def on_start(self):
+    #    self.client.get("/marcas-y-productos/")
 
     #@task
     #def index(self):
     #    self.client.get("/marcas-y-productos/")
     #    #self.client.get("/static/assets.js")
         
-    #@task
-    #def about(self):
-    #    self.client.get("/about/")
+    @task
+    def about(self):
+        self.client.get("/marcas-y-productos/")
